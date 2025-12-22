0927d619bb68aec5caa45651b9237bd53b159f12
Alex J Keyser <alexanderjkeyser@gmail.com>
2025-12-22 19:25:38 +0000

    Add files via upload

---
+++ BaseConverter.cs
@@ -0,0 +1,195 @@
+﻿/*
+TCreative Commons Attribution-NonCommercial 4.0 International
+(CC BY-NC 4.0)
+
+Copyright © 2025 Alexander Keyser
+https://Website-ajk.uk
+
+This project is licensed under the Creative Commons
+Attribution-NonCommercial 4.0 International License.
+
+Full license text:
+https://creativecommons.org/licenses/by-nc/4.0/
+Or open the LICENSE File within this project
+*/
+
+/*
+ "BaseConverter" is a C# library that can convert between base 2, 10, and 16
+ This is Version 1.0
+*/
+
+namespace BaseConverter;
+public class BaseConverter
+{ 
+        #region IntToBinary
+        public static string Base10_To_2(byte Integer)
+        {
+            List<byte> list = new List<byte>();
+            byte bit;
+            list.Add(0);
+            for (int i = 0; Integer > 0; i++)
+            {
+                bit = Convert.ToByte(Integer % 2);
+                list.Add(bit);
+                Integer = Convert.ToByte(Integer / 2);
+            }
+            string FinalBinary = string.Empty;
+            for (int i = list.Count -1; i > 0; i--)
+            {
+                FinalBinary += list[i];
+            }
+
+            return FinalBinary;
+        }
+        public static string Base10_To_2(Int16 Integer)
+        {
+            List<Int16> list = new List<Int16>();
+            Int16 bit;
+            list.Add(0);
+            for (int i = 0; Integer > 0; i++)
+            {
+
+                bit = Convert.ToInt16(Integer % 2);
+                list.Add(bit);
+                Integer = Convert.ToInt16(Integer / 2);
+            }
+            string FinalBinary = string.Empty;
+            for (int i = list.Count -1; i > 0; i--)
+            {
+                FinalBinary += list[i];
+            }
+
+            return FinalBinary;
+        }
+        public static string Base10_To_2(Int32 Integer)
+        {
+            List<Int32> list = new List<Int32>();
+            Int32 bit;
+            list.Add(0);
+            for (int i = 0; Integer > 0; i++)
+            {
+                bit = Integer % 2;
+                list.Add(bit);
+                Integer = Integer / 2;
+            }
+            string FinalBinary = string.Empty;
+            for (int i = list.Count -1; i > 0; i--)
+            {
+                FinalBinary += list[i];
+            }
+
+            return FinalBinary;
+        }
+        public static string Base10_To_2(Int64 Integer)
+        {
+            List<Int64> list = new List<Int64>();
+            Int64 bit;
+            list.Add(0);
+            for (int i = 0; Integer > 0; i++)
+            {
+                bit = Integer % 2;
+                list.Add(bit);
+                Integer = Integer / 2;
+            }
+            string FinalBinary = string.Empty;
+            for (int i = list.Count -1; i > 0; i--)
+            {
+                FinalBinary += list[i];
+            }
+
+            return FinalBinary;
+        }
+        public static string Base10_To_2(Int128 Integer)
+        {
+            List<Int128> list = new List<Int128>();
+            Int128 bit;
+            list.Add(0);
+            for (int i = 0; Integer > 0; i++)
+            {
+                bit = Integer % 2;
+                list.Add(bit);
+                Integer = Integer / 2;
+            }
+            string FinalBinary = string.Empty;
+            for (int i = list.Count -1; i > 0; i--)
+            {
+                FinalBinary += list[i];
+            }
+
+            return FinalBinary;
+        }
+        #endregion
+
+        #region IntToHex
+        public static string Base10_To_16(byte Integer)
+        {
+            return Convert.ToString(Integer, 16);
+        }
+        public static string Base10_To_16(Int16 Integer)
+        {
+            return Convert.ToString(Integer, 16);
+        }
+        public static string Base10_To_16(Int32 Integer)
+        {
+            return Convert.ToString(Integer, 16);
+        }
+        public static string Base10_To_16(Int64 Integer)
+        {
+            return Convert.ToString(Integer, 16);
+        }
+        #endregion
+
+        #region BinaryToInt
+        public static byte Base2_To_10_Byte(string Binary)
+        {
+            return Convert.ToByte(Binary, 2);
+        }
+        public static Int16 Base2_To_10_Int16(string Binary)
+        {
+            return Convert.ToInt16(Binary, 2);
+        }
+        public static Int32 Base2_To_10_Int32(string Binary)
+        {
+            return Convert.ToInt32(Binary, 2);
+        }
+        public static Int64 Base2_To_10_Int64(string Binary)
+        {
+            return Convert.ToInt64(Binary, 2);
+        }
+        #endregion
+
+        #region BinaryToHex
+        public static string BinaryToHex(string Binary)
+        {
+            return Base10_To_16(Base2_To_10_Int64(Binary));
+        }
+        #endregion
+
+        #region HexToInt
+        public static byte Base16_To_10_Byte(string Hex)
+        {
+            return Convert.ToByte(Hex, 16);
+        }
+        public static Int16 Base16_To_10_Int16(string Hex)
+        {
+            return Convert.ToInt16(Hex, 16);
+        }
+        public static Int32 Base16_To_10_Int32(string Hex)
+        {
+            return Convert.ToInt32(Hex, 16);
+        }
+        public static Int64 Base16_To_10_Int64(string Hex)
+        {
+            return Convert.ToInt64(Hex, 16);
+        }
+        #endregion
+
+        #region HexToBinary
+        public static string Base16_To_2(string Hex)
+        {
+            return Convert.ToString(Base16_To_10_Int64(Hex), 2);
+            
+        }
+        #endregion
+
+}
---
+++ README.md
@@ -0,0 +1,96 @@
+#### -------Documentation-------
+
+
+
+* Method: Base10\_To\_2() \[IntToBinary] Parameters: Integer +5 overloads (byte, Int16, Int32, Int64, Int128) Return vlaue: string Binary
+
+
+
+* Method: Base10\_To\_16() \[IntToHex] Parameters: Integer +4 overloads (byte, Int16, Int32, Int64) Return vlaue: string Hex
+
+
+
+
+
+* Method: Base2\_To\_10() \[BinaryToIntByte, BinaryToInt16, BinaryToInt32, BinaryToInt64] Parameters: string Binary Return vlaue: Byte, Int16, Int32, Int64
+
+
+
+* Method: Base2\_To\_16() \[BinaryToHex] Parameters: string Binary Return vlaue: string Hex
+
+
+
+
+
+* Method: Base16\_To\_10() \[HexToIntByte, HexToInt16, HexToInt32, HexToInt64] Parameters: string Hex Return vlaue: Byte, Int16, Int32, Int64
+
+
+
+* Method: Base16\_To\_2() \[HexToBinary] Parameters: string Binary Return vlaue: string Hex
+
+
+
+
+
+
+
+
+
+#### -------Copyright info-------
+
+
+
+Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)
+
+
+
+Copyright © 2025 Alexander Keyser https://Website-ajk.uk
+
+
+
+This project is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+
+
+
+You are free to:
+
+
+
+Share — copy and redistribute the material in any medium or format
+
+Adapt — remix, transform, and build upon the material
+
+Under the following terms:
+
+
+
+Attribution — You must give appropriate credit to the original author, provide a link to the license, and indicate if changes were made.
+
+NonCommercial — You may not use the material for commercial purposes.
+
+No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+
+
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+
+
+
+Full license text: https://creativecommons.org/licenses/by-nc/4.0/ Or open the LICENSE File within this project
+
+
+
+
+
+
+
+
+
+#### -------Extra info-------
+
+
+
+This is my first library which consists of a few different ways to convert binary, hexadecimal, and integer values together. It isn't the prettiest code in the world, it is code that works.
+
+
+
